ca14090f82def3ab67e8e24b7fdffc5f1ba98739
Shivam Gupta <guptashivam3935@gmail.com>
2025-11-30 10:03:30 -0800

    adding basic react folder and some changes

---
+++ react_revision/src/App.jsx
@@ -0,0 +1,13 @@
+import Ice from "./Ice"
+
+function App() {
+  
+  return (
+    <>
+      <h1>chai aur react | shivam gupta</h1>    
+      <Ice/>
+    </>
+  )
+}
+
+export default App
---
+++ react_revision/src/Ice.jsx
@@ -0,0 +1,7 @@
+function Ice(){
+    return(
+        <h2>Ice tea is different</h2>
+    )
+}
+
+export default Ice
